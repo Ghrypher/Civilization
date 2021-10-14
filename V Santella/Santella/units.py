d705ec5eb9b4e3ceeedbe32b2569b4e19c0827fb
Ghrypher <codicegamer@gmail.com>
2021-10-13 21:03:00 -0300

    the final worls assets are added

--- V Santella/Santella/units.py
+++ V Santella/Santella/units.py
@@ -1,11 +1,16 @@
 class Unit():
 
-    def __init__(self):
+    def __init__(self, type):
         """Inicio de la clase, crea las variables de la misma"""
         self.life = None
         self.team = None
         self.positionX = None
         self.positionY = None
+        self.define(type)
+
+    def define(self, type):
+        """" """
+    
 
     def setPosition(self, posX, posY):
         self.positionX = posX
