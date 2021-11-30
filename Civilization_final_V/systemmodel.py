8d9508ff72c5f963a8f0f944fb0e1fc1d9c2c438
unknown <isanna@alumno.huergo.edu.ar>
2021-11-30 17:56:52 -0300

    Cleared the master branch

--- Civilization_final_V/systemmodel.py
+++
@@ -1,7 +0,0 @@
-import random, sys, copy, os, time
-from gameboard import GameBoard
-from character import Character
-from systemcontroller import SystemController
-
-class SystemModel():
-    """"""
